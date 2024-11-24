574a9c77a1df96335ace071ecb373be550fb0b60
raja-delhi <nonofficial1234098@gmail.com>
2024-11-24 20:55:16 +0530

    Added set reject flag true on referral approve.

--- src/main/java/com/earnwithfun/controller/MainController.java
+++ src/main/java/com/earnwithfun/controller/MainController.java
@@ -220,7 +220,7 @@ public class MainController {
     public RedirectView approveReferralRequest(@ModelAttribute User mainUser, HttpServletRequest request, RedirectAttributes redirectAttributes){
         mainUser = userService.getUserById(mainUser.getId());
         mainUser.setReferralRequest('N');
-        mainUser.setIsRejectedByAdmin('Y');
+        mainUser.setIsRejectedByAdmin('N');
         this.userService.updatePayments(mainUser, mainUser.getPaymentPlan(), "Login bonus");
         this.userService.updateReferralRewardPoints(mainUser);
         RedirectView redirectView = new RedirectView();
